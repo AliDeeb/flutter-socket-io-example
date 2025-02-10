fd1cd7f5b008ba7e1e0e158288d1061182d27226
Ali Deeb <ali1alideeb@gmail.com>
2025-02-10 13:55:27 +0300

    refactoring.

---
+++ app/lib/app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Socket',
+      home: MyHomePage(title: 'Flutter Demo Socket'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+    );
+  }
+}
